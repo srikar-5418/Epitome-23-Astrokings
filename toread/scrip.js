6f475ff6ad8a7f6e69e628ed6f04694d9004cc5f
srikar-5418 <93670893+srikar-5418@users.noreply.github.com>
2023-02-11 23:26:50 +0530

    Add files via upload

---
+++ toread/scrip.js
@@ -0,0 +1,12 @@
+let dis=document.getElementsByClassName("discover");
+// dis.onclick=function(){
+//     alert("yes");
+//  };
+ dis.addEventListner('click',function(e){
+ alert("hi");
+})
+var ser=document.getElementById("Searchbar");
+ser.addEventListener('click',function(e){
+    alert("hi");
+});
+
